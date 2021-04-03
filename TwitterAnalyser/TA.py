05654956be430f4c999d0ab3b5797e7865f1b409
Tejas B Achar <30596600+Tejas-Achar@users.noreply.github.com>
2021-04-03 20:40:41 +0530

    Update TA.py

--- TwitterAnalyser/TA.py
+++ TwitterAnalyser/TA.py
@@ -1,7 +1,9 @@
+# import libraries -------------------------------
 from tkinter import*
 from textblob import TextBlob
 import tweepy
 
+# get apikey and secret keys from text file ------
 mykeys = open("auth4","r").read().splitlines()
 api_key = mykeys[0]
 api_key_secret = mykeys[1]
@@ -11,12 +13,15 @@ api_token_secret = mykeys[3]
 auth_handler = tweepy.OAuthHandler(consumer_key=api_key, consumer_secret=api_key_secret)
 auth_handler.set_access_token(api_token,api_token_secret)
 
+# initialize api------------------------------------
 api = tweepy.API(auth_handler,wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
 
-
+# initialize window---------------------------------
 root = Tk()
 root.geometry('500x500')
 root.title("TA")
+
+# Gui elements -------------------------------------
 label_0 = Label(root, text="Twitter Analyser",width=20,font=("bold", 20))
 label_0.place(x=90,y=53)
 label_1 = Label(root, text="Search Query",width=20,font=("bold", 10))
@@ -29,25 +34,33 @@ entry_2 = Entry(root)
 entry_2.place(x=240,y=180)
 label_3 = Label(root, text="Result : ",width=20,font=("bold", 10))
 label_3.place(x=68,y=230)
+
+#label to display result ------------------------------------------
 label_4 = Label(root, text="",width=65,font=("bold", 10))
 label_4.place(x=7,y=260)
 
 
+# ----Main metho to perform tex analysis---------------------------
 def call_result(label_result, entry_1, entry_2):
-    search_term = (entry_1.get())
-    tweet_amount = int(entry_2.get())
-    result = ""
-
+    search_term = (entry_1.get()) #-------------get search term----
+    tweet_amount = int(entry_2.get()) #---------tweet amount-------
+    # call api-----------------------------------------------------
     tweets = tweepy.Cursor(api.search, q=search_term, lang="en").items(tweet_amount)
 
     polarity = 0
     positive = 0
     neutral = 0
     negative = 0
+    
+    # Initialize lists to add positive negative and neutral tweets-
     positiveTweets = []
     negativeTweets = []
     neutralTweets = []
+    
+    # Loop over obtained tweets -----------------------------------
     for tweet in tweets:
+        
+        #format text ----------------------------------------------
         final_text = tweet.text.replace('RT', '')
         if final_text.startswith(' @'):
             position = final_text.index(':')
@@ -57,6 +70,8 @@ def call_result(label_result, entry_1, entry_2):
             position = final_text[position + 2:]
         analysis = TextBlob(final_text)
         tweet_polarity = analysis.polarity
+        
+        # conditions to check whether tweets are positive or negative----------
         if tweet_polarity > 0:
             print(type(final_text))
             positiveTweets.append(final_text)
@@ -65,18 +80,17 @@ def call_result(label_result, entry_1, entry_2):
             negativeTweets.append(final_text)
             negative += 1
         else:
-            # f = open("Neutral.txt", "w+").writelines(str(final_text))
             neutralTweets.append(final_text)
             neutral += 1
 
         polarity += analysis.polarity
 
-    # print(polarity)
 
     x = "Positive tweets = "+str(positive)
     y = "Negative tweets = "+str(negative)
     z = "Neutral tweets = "+str(neutral)
-
+    
+    # Write data to textfiles----------------------------------
     f1 = open("Positive.txt", "w+")
     f1.writelines(positiveTweets)
     f1.close()
@@ -94,7 +108,7 @@ def call_result(label_result, entry_1, entry_2):
     label_result.config(text=result)
     return
 
-
+# Call function on button press
 Button(root, text='Submit',width=20,bg='brown',fg='white',command=lambda:call_result(label_result=label_4,entry_1=entry_1,entry_2=entry_2)).place(x=180,y=380)
-# it is use for display the registration form on the window
+# run main window
 root.mainloop()
